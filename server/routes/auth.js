76c1ce6ef6979c6799c2a58b313dfd7cbefff7bc
mar1n <30738216+mar1n@users.noreply.github.com>
2021-04-21 10:07:51 +0100

    create routes

---
+++ server/routes/auth.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/signup', (req, res) => {
+    res.json({
+        data: 'you hit signup end point'
+    })
+});
+
+module.exports = router;
